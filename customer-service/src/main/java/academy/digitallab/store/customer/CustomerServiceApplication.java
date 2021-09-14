643447a2a326bfd20513a0bf744ebdabb99e1783
maicol783 <maicolocampoareiza783@gmail.com>
2021-09-14 14:14:31 -0500

    prueba

--- customer-service/src/main/java/academy/digitallab/store/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/academy/digitallab/store/customer/CustomerServiceApplication.java
@@ -2,7 +2,7 @@ package academy.digitallab.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class CustomerServiceApplication {
